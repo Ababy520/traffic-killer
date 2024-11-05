0c8555a0a6f8eb74f821d630bd94511e36173d37
Ababy520 <132305359+Ababy520@users.noreply.github.com>
2024-11-05 16:12:11 +0800

    Update main.js

--- app/main.js
+++ app/main.js
@@ -334,14 +334,6 @@ option = {
             type: 'line',
             data: [{
                 name: new Date(),
-                value: now_local_ping
-            }]
-        },
-        {
-            name: '延迟',
-            type: 'line',
-            data: [{
-                name: new Date(),
                 value: now_global_ping
             }]
         }
