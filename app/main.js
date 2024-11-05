371b7e4cee09d6382b6a316d6c4eb06dd94cd23c
Ababy520 <132305359+Ababy520@users.noreply.github.com>
2024-11-05 11:59:58 +0800

    Update main.js

--- app/main.js
+++ app/main.js
@@ -164,7 +164,7 @@ function ipcn() {
             .then(response => response.json())
             .then(data => {
                 var tag = document.getElementById("ipcn")
-                tag.innerText = data['remote_addr'] + ' ' + data.remote_addr_location.country + data.remote_addr_location.city + ' ' + data.remote_addr_location.isp
+                tag.innerText = data['remote_addr'] + ' ' + data.remote_addr_location.province + ' ' + data.remote_addr_location.city + ' ' + data.remote_addr_location.isp
                 if (data['remote_addr'] !== cnip) {
                     tag.style.color = ''
                     ckip(data['remote_addr'], tag)
